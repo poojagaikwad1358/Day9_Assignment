91f4e60238f201b300dd8a13d902e1f685f88ed3
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-03 16:06:11 +0530

    Delete

--- empPresentOrNot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-isPresent=1;
-randomCheck=$(( RANDOM%2 ))
-if [ $randomCheck -eq $isPresent ]
-then
-        echo "Employee is Present";
-else
-        echo "Employee is Absent";
-fi

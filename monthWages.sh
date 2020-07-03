92405995f3185d579684afc53819512bea582e29
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-03 16:58:57 +0530

    Merge this file from U5-monthWages to master branch

---
+++ monthWages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+partTime=1
+fullTime=2
+totalSalary=0;
+empRatePerHrs=20;
+numberOfWorkingDays=20;
+
+for(( Day=1; Day<=$numberOfWorkingDays; Day++ ))
+do
+	randomCheck=$(($RANDOM%3));
+	case $randomCheck in
+		$fullTime)
+				empHrs=8
+				;;
+		$partTime)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+	esac
+
+salary=$(($empHrs*$empRatePerHrs));
+totalSalary=$(($totalSalary+$salary))
+
+done
+echo "Employee monthly salary: " $totalSalary;
